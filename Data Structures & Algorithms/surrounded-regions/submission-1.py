67a8d8054e2b9a083a75bd2cdd89caa2d4d7e11a
aaldelaimy <awaldelaimy@gmail.com>
2026-07-17 15:56:34 -0700

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        
+        ROWS, COLS = len(board), len(board[0])
+        safe = set()
+
+        def dfs(r, c):
+            if (
+                r < 0 or r >= ROWS or
+                c < 0 or c >= COLS or
+                (r, c) in safe or
+                board[r][c] == "X"
+            ):
+                return
+
+            safe.add((r, c))
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(ROWS):
+            dfs(r, 0)
+            dfs(r, COLS - 1)
+        
+        for c in range(COLS):
+            dfs(0, c)
+            dfs(ROWS - 1, c)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O" and (r, c) not in safe:
+                    board[r][c] = "X"
+        
